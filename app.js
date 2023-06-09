932001ac80b856078fdea31dbc7c0aabdde8fd21
Priyanshu Pandya <priyanshupandya20112003@gmail.com>
2023-06-09 18:18:16 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+/*
+
+File Name:  app.js
+Author:     Priyanshu Pandya
+Date:       09-06-2023
+Student ID: 301280909
+
+*/
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Define routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/services', (req, res) => {
+  res.render('services');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
